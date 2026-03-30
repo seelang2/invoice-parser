52916e14ccc2874d489d73223db34e3a11a076aa
seelang2 <seelang2@gmail.com>
2026-03-30 09:09:02 -0500

    Update app structure

--- parser-frontend/src/main.tsx
+++ parser-frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./app/App.tsx";
 
 // Adjusted for strict null checks
 const rootElem = document.getElementById("root");
